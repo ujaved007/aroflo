adad1418897f63174c0239215dd1347af987de1d
Sam Jav <ujaved007@gmail.com>
2022-02-26 21:03:31 +1100

    initial setup

---
+++ src/styles/GlobalStyles.styles.js
@@ -0,0 +1,50 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+
+    @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap");
+    html,
+    body {
+	background: #F0F0F1;
+    height: 100%;
+	font-family: "Roboto", sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+}
+h1 {
+    font-size: 1.8rem;
+    font-weight: 500
+}
+h2 {
+    font-size: 1.4rem;
+    font-weight: 500
+}
+h3 {
+    font-size: 1.2rem;
+}
+p {
+    font-size: 1rem;
+}
+li {
+    font-size: 1rem;
+}
+@media (max-width: 768px) {
+    h1 {
+    font-size: 1.6rem;
+    }
+    h2 {
+    font-size: 1.2rem;
+    }
+    h3 {
+    font-size: 1rem;
+    }
+    p {
+    font-size: 0.9rem;
+    }
+    li {
+    font-size: 0.9rem;
+}
+}
+    `;
+
+export default GlobalStyles;
